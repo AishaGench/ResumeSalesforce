255981d641a3f037c7632307013ff8ac82a97ab6
Aisha Gench <aisha.gench@gmail.com>
2023-09-08 17:39:26 -0500

    created myResumeSummary component

--- force-app/main/default/lwc/myResumeContainer/myResumeContainerData.js
+++ force-app/main/default/lwc/myResumeContainer/myResumeContainerData.js
@@ -28,3 +28,8 @@ export const USER_DETAILS={
     EMAIL:'aisha.gench@gmail.com',
     PHONE:'832 552 0102'
 }
+export const CAREER_SUMMARY={
+    HEADING:"CAREER SUMMARY",
+    DESCRIPTION: "I have 5+ years in the IT industry and experience as a Salesforce Developer. I've been very closely involved in the entire salesforce development lifecycle within the CI/CD pipeline, application design patterns, and integration patterns. When it comes to the tools, I fully take advantage of triggers, Apex classes, integration services, controllers, as well as Aura framework and lightning web components. I am also very much hands-on in the integration part of Salesforce, using Rest and Soap APIs. Highly skilled and dedicated software professional with expertise in object-oriented programming (Java) and test automation frameworks. Proficient in software development methodologies, particularly Agile/Scrum, with designing, developing, and executing automation scripts. Dedicated, highly motivated, and analytical Problem-Solver. A strong believer in teamwork, and collaboration with a cross-functional team mindset.",
+   
+}
